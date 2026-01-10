bfe3f65594b436c25ab8515e86b07680e9aa6f69
shahana sherin <senurashik@gmail.com>
2026-01-11 00:34:57 +0530

    Fixed Dashboard errors and Clerk Protect misuse

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { dummyCreationData } from '../assets/assets'
+import { Gem, Sparkles } from 'lucide-react'
+import { Protect } from '@clerk/clerk-react'
+import CreationItems from '../components/CreationItems'
 
 const Dashboard = () => {
 
@@ -17,7 +20,50 @@ useEffect(()=>{
     <div className='h-full overflow-y-scroll p-6'>
       <div className='flex justify-start gap-4 flex-wrap'>
 {/*Total Creation Cards*/}
+<div className='flex justify-between items-center w-72 p-4 px-6 bg-white 
+rounded-xl border border-gray-200'>
+    <div className='text-slate-600'>
+        <p className='text-sm'>Total Creations</p>
+        <h2 className='text-xl font-semibold'>{creations.length}</h2>
+    </div>
+<div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2]
+to-[#0BB0D7] text-white flex justify-center items-center'>
+    <Sparkles className='w-5 text-white'/>
+</div>
+</div>
+{/*Active Creation Cards*/}
+
+<div className='flex justify-between items-center w-72 p-4 px-6 bg-white 
+rounded-xl border border-gray-200'>
+    <div className='text-slate-600'>
+        <p className='text-sm'>Active Creations</p>
+        <h2 className='text-xl font-semibold'>
+          <Protect
+  condition={(has) => has({ role: "premium" })}
+  fallback="Free"
+>
+  Premium
+</Protect>
+
+        </h2>
+    </div>
+<div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5]
+to-[#9E53EE] text-white flex justify-center items-center'>
+    <Gem  className='w-5 text-white'/>
+</div>
+</div>
+
       </div>
+
+
+<div className='space-y-3'>
+<p className='mt-6 mb-4'>Recent Creations</p>
+{
+    creations.map((item)=> <CreationItems key={item.id} item={item}/>)
+}
+</div>
+
+
     </div>
   )
 }
